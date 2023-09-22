7dd3eb703c17ae187db013bcfa1a379f97b4402a
RohiV04 <rohith_csd@srkrec.edu.in>
2023-09-22 21:29:19 +0530

    added cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 mongoose.set("strictQuery", true);
-const url =process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL;
 const port = process.env.PORT || 9000;
 mongoose.connect(url);
 const con = mongoose.connection;
@@ -14,24 +14,19 @@ con.once("open", () => {
   console.log("Mongo DB connected");
 });
 
-
 app.use(express.json());
-app.get('/',(req,res)=>{
-  res.send('Welcome')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
 const linkRouter = require("./routes/history");
 // const testRouter = require("./router/test");
-const translateRouter=require("./routes/index");
+const translateRouter = require("./routes/index");
 app.use("/", translateRouter);
 app.use("/", linkRouter);
 // app.use("/", testRouter);
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
 
 app.listen(port, () => {
