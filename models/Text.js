632377089f4dca3f4606b0a1f6baa5b04f860d02
RohiV04 <rohith_csd@srkrec.edu.in>
2023-10-02 13:43:53 +0530

    updated History

--- models/Text.js
+++ models/Text.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose');
-const textSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const textSchema = new mongoose.Schema(
+  {
     fileUrl: {
-        type: String,
-        required: true
-    },
-    text: {
       type: String,
-      required: true
-    }
-  }, {
-    timestamps: true // enable automatic timestamp fields
-  });
+      required: true,
+    },
+    extracted: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+      required: true,
+    },
+    translated: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // enable automatic timestamp fields
+  }
+);
 const Text = mongoose.model("Text", textSchema);
 module.exports = Text;
